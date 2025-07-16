8f06609a9bf28e65482b1863c78fc452227c2150
albertombundi <albertombundi@outlook.com>
2025-07-16 22:26:30 +0100

    rotas vue & Home view

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import './assets/main.css';
 import 'primeicons/primeicons.css';
+import routes from './router';
 
 import { createApp } from 'vue'
 import App from './components/App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(routes);
+
+app.mount('#app');
