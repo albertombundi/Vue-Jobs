76d2df0c233568a47d6c445ade8fa768b4cdf6a8
albertombundi <albertombundi@outlook.com>
2025-07-30 21:59:07 +0100

    working in NotfoundPage.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const routes = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,10 +18,15 @@ const routes = createRouter({
             component: JobsView,
         },
         {
+            path: "/jobs/:id",
+            name: "job",
+            component: JobView,
+        },
+        {
             path: '/:catchAll(.*)',
             name: "not-found",
             component: NotFoundView,
-        }
-    ]
-})
+        },
+    ],
+});
 export default routes;
