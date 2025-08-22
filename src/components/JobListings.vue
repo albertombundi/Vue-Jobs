5369d7db38b017068e697b6823841a405778c7ba
albertombundi <albertombundi@outlook.com>
2025-08-22 12:17:25 +0100

    JobsListings Reator to reactive() function

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,27 +1,32 @@
 <script setup>
-    import JobListing from './JobListing.vue';
-    import { ref, defineProps, onMounted } from 'vue';
-    import axios from 'axios';
+import JobListing from './JobListing.vue';
+import { reactive, defineProps, onMounted } from 'vue';
+import axios from 'axios';
 
-    defineProps({
-        limit: Number,
-        showButton: {
-            type: Boolean,
-            default: false
-        },
-    });
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    },
+});
 
-    const jobs = ref([]);
+const state = reactive({
+    jobs: [],
+    isLoading: true,
 
-    onMounted( async () => {
-        try {
-            const response = await axios.get('http://localhost:5000/jobs');
-            jobs.value = response.data;
-        } catch (error) {
-            console.error('Error fetching jobs:', error);
-        }
-    });
+});
 
+onMounted( async () => {
+    try {
+        const response = await axios.get('http://localhost:5000/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Error fetching jobs:', error);
+    } finally {
+        state.isLoading = false;
+    }
+});
 </script>
 
 <template>
@@ -31,9 +36,10 @@
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" 
-                :key="job.id" 
-                :job="job" 
+                <JobListing 
+                 v-for="job in state.jobs.slice(0, limit || state.jobs.length)" 
+                 :key="job.id" 
+                 :job="job" 
             />
             </div>
         </div>
